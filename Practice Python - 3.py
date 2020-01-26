9ccdcf1cf5835a93b6f24ec3d7fb88298893da5c
Sebastian <sebasgrammar@hotmail.com>
2020-01-26 13:57:25 -0500

    Added a one-liner for exercise 3 - extra

---
+++ Practice Python - 3.py
@@ -0,0 +1,17 @@
+"""
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+belowFive = []
+
+for number in a:
+    if number < 5:
+        print(number)
+        belowFive.append(number)
+
+
+print(belowFive)
+"""
+
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+belowFive = list(filter(lambda x: x < 5, a))
+print(belowFive)
