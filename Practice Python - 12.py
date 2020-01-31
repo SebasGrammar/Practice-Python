451abe0cc46221e898d6010e4ca2167bf3fef16d
Sebastian <sebasgrammar@hotmail.com>
2020-01-31 00:06:43 -0500

    added a comment on higher order functions

--- Practice Python - 12.py
+++ Practice Python - 12.py
@@ -35,3 +35,21 @@ def doThat(x):
     print(x)
 
 doThat(generateList())
+
+"""
+doThat(generateList) (1)
+vs
+doThat(generateList()) (2)
+
+In (1), we are just passing in the reference to a function, while in (2)
+we are actually calling the function and using whatever value it returns
+as an argument for the outer function.
+
+This is different from what happens with the doThis function, in which the
+function passed in as an argument is being called from the body of doThis.
+The problem with this is that if we wanted to pass in any arguments to the inner function,
+we would have to make that clear by passing them in the function call. If we decided that's not what we want,
+we would have to change the body of the function. = not so flexible.
+"""
+
+
